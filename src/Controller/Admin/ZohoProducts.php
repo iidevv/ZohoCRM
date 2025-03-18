48efbcfd67a7b6a4b31b369cfd6b62c6048f5558
spanandequipment <ilya.i@skinact.com>
2025-03-18 10:28:32 -0700

    removed manual controls

--- src/Controller/Admin/ZohoProducts.php
+++ src/Controller/Admin/ZohoProducts.php
@@ -2,41 +2,7 @@
 
 namespace Iidev\ZohoCRM\Controller\Admin;
 
-use Iidev\ZohoCRM\Core\Dispatcher\CreateProductsDispatcher;
-use Iidev\ZohoCRM\Core\Dispatcher\CreateProductVariantsDispatcher;
-use Iidev\ZohoCRM\Core\Dispatcher\UpdateProductsDispatcher;
-use Iidev\ZohoCRM\Core\Dispatcher\UpdateProductVariantsDispatcher;
-use XLite\Core\Converter;
-
 class ZohoProducts extends Zoho
 {
-    protected function doActionCreateZohoProducts()
-    {
-        $dispatcherProducts = new CreateProductsDispatcher();
-        $message    = $dispatcherProducts->getMessage();
-        
-        $this->bus->dispatch($message);
 
-        $dispatcherVariants = new CreateProductVariantsDispatcher();
-        $message    = $dispatcherVariants->getMessage();
-        
-        $this->bus->dispatch($message);
-
-        $this->setReturnURL(Converter::buildURL(\Iidev\ZohoCRM\View\Tabs\Zoho::TAB_PRODUCTS));
-    }
-
-    protected function doActionUpdateZohoProducts()
-    {
-        $dispatcherProducts = new UpdateProductsDispatcher();
-        $message    = $dispatcherProducts->getMessage();
-        
-        $this->bus->dispatch($message);
-
-        $dispatcherVariants = new UpdateProductVariantsDispatcher();
-        $message    = $dispatcherVariants->getMessage();
-        
-        $this->bus->dispatch($message);
-
-        $this->setReturnURL(Converter::buildURL(\Iidev\ZohoCRM\View\Tabs\Zoho::TAB_PRODUCTS));
-    }
 }
--- src/Controller/Admin/ZohoProfiles.php
+++ src/Controller/Admin/ZohoProfiles.php
@@ -2,18 +2,7 @@
 
 namespace Iidev\ZohoCRM\Controller\Admin;
 
-use Iidev\ZohoCRM\Core\Dispatcher\CreateProfilesDispatcher;
-use XLite\Core\Converter;
-
 class ZohoProfiles extends Zoho
 {
-    protected function doActionCreateZohoProfiles()
-    {
-        $dispatcher = new CreateProfilesDispatcher();
-        $message    = $dispatcher->getMessage();
-
-        $this->bus->dispatch($message);
-
-        $this->setReturnURL(Converter::buildURL(\Iidev\ZohoCRM\View\Tabs\Zoho::TAB_PROFILES));
-    }
+    
 }
